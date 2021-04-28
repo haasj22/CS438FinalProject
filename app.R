079b2f82fdd56a510b2d659350d1a1235e9a2817
Ashley <kima21@up.edu>
2021-04-27 21:56:05 -0700

    Accidently deleted some work! Re-added

--- app.R
+++ app.R
@@ -17,39 +17,103 @@ library(tree)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Math Team Final Dashboard"),
-    h2("Choose variables to predict the amount of math majors at a college"),
-
-    sidebarLayout(
-        sidebarPanel(
-            radioGroupButtons("in_state_tuition_choice", label = h6("Students paying in-state Tuition:"), 
-                         choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("out_of_state_tuition_choice", label = h6("Students paying out-of-State Tuition:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("percentage_of_women_choice", label = h6("Students that are women:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("graduation_rate_choice", label = h6("Students that graduate in 4 year institutions:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("part_time_choice", label = h6("Part time student workers:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("white_student_choice", label = h6("Students who are white:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("multirace_choice", label = h6("Students who are two or more races:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
-            radioGroupButtons("federal_loan_choice", label = h6("Students who are receiving federal loans:"), 
-                              choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE)
-            
+    
+    navbarPage("MATH Team Project",
+        tabPanel(
+            "Model Creation Page",
+            # Application title
+            titlePanel("Create your own models using our cleaned dataset"),
+            h2("Choose variables to predict the amount of math majors at a college"),
+            # Sidebar with a slider input for number of bins 
+            sidebarLayout(
+                sidebarPanel(
+                    radioGroupButtons("in_state_tuition_choice", label = h6("In State Tuition:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("out_of_state_tuition_choice", label = h6("Out of State Tuition:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("percentage_of_women_choice", label = h6("Percentage of women:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("graduation_rate_choice", label = h6("Graduation rate at 4 year insitutions:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("part_time_choice", label = h6("Percentage of students who are part time workers:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("white_student_choice", label = h6("Percentage of students who are white:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("multirace_choice", label = h6("Percentage of students who are two or more races:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE),
+                    radioGroupButtons("federal_loan_choice", label = h6("Percentage of students who are receiving federal loans:"), 
+                        choices = list("Yes" = 1, "No" = 2), selected = 2, direction="horizontal", individual=FALSE)
+                ),
+                           
+                # Show a plot of the generated distribution
+                mainPanel(
+                    verticalLayout(
+                    verbatimTextOutput("linear_regression"),
+                    plotOutput("tree")
+                    )
+                )
+            ),
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           verticalLayout(
-               verbatimTextOutput("linear_regression"),
-               plotOutput("tree")
-           )
-        )
+        tabPanel(
+            "Model Display Page",
+            titlePanel("Test some values on our best model"),
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput("graduation_rate_to_predict", 
+                        "School's graduation rate",
+                        min=0.0, 
+                        max=1.0, 
+                        step=0.01, 
+                        value=0.5),
+                    sliderInput("in_state_tuition_to_predict", 
+                        "School's cost for in state tution",
+                        min=0, 
+                        max=60000, 
+                        step=100, 
+                        value=20000),
+                    sliderInput("out_state_tuition_to_predict", 
+                        "School's cost for out of state tution",
+                        min=0, 
+                        max=60000, 
+                        step=100, 
+                        value=20000),
+                    sliderInput("part_time_job_percent_to_predict",
+                        "Percentage of school's students that work part time jobs",
+                        min=0.0, 
+                        max=1.0, 
+                        step=0.01, 
+                        value=0.5),
+                    sliderInput("white_student_percentage_to_predict",
+                        "Percentage of students who are white",
+                        min=0.0, 
+                        max=1.0, 
+                        step=0.01, 
+                        value=0.5)
+                    ),
+                               
+                    mainPanel(
+                        verbatimTextOutput("best_tree"),
+                        h4("Model Analysis"),
+                        p("As shown in this model, we our best model explains 33% percent of the variance.
+                      While this is not an amazing result the model does shed light on a couple interesting correlations.
+                      First the positive coefficients for the out of state tuition fee means that the variable 
+                      has a positive correlation with the percentage of math majors.
+                      Perhaps this is because schools that cost more have higher funding for their programming or 
+                      it could mean that these colleges just spend more money on advertizing."),
+                        p("In contrast, colleges with a higher percentage of white students have a lower percentage of math majors.
+                      We do not know how to explain this trend, but maybe white students are more likely to choose other majors."),
+                        p("Next, it's relieving for students that like math to know that colleges with higher graduation rates
+                      tend to have more math majors."),
+                        p("Finally there is fluctuation for what an increase in part time workers does to the amount of math majors.
+                      Initial increases in part time labor show a decrease in math majors, but continued increase shows a rebound in math major percentages."),
+                        p("The last thing of note is that the attributes of the female percentage of students, 
+                      the proportion of multirace students, and the ability of students to get loans showed
+                      no significance when trying to predict the number of math majors."),
+                        h4("Your predicted college's percentage of math majors according to our model."),
+                        verbatimTextOutput("prediction")
+                    )
+                )
+            )
     )
 )
 
@@ -58,6 +122,7 @@ server <- function(input, output) {
     
     filedata <- read.csv("MERGED2018_19_PP.csv", header=TRUE)
     
+    #variables used to create models
     inState <- reactive({input$in_state_tuition_choice})
     outState <- reactive({input$out_of_state_tuition_choice})
     percWomen <- reactive({input$percentage_of_women_choice})
@@ -66,6 +131,22 @@ server <- function(input, output) {
     whiteStudent <- reactive({input$white_student_choice})
     multiStudent <- reactive({input$multirace_choice})
     federal <- reactive({input$federal_loan_choice})
+    
+    #variables used to predict off our best model
+    gradRateToPredict <- reactive({input$graduation_rate_to_predict})
+    inStateToPredict <- reactive({input$in_state_tuition_to_predict})
+    outStateToPredict <- reactive({input$out_state_tuition_to_predict})
+    partTimeToPredict <- reactive({input$part_time_job_percent_to_predict})
+    whiteStudentToPredict <- reactive({input$white_student_percentage_to_predict})
+    
+    data_for_best_model <- filedata[, c("C150_4", "TUITIONFEE_IN", 
+                                        "TUITIONFEE_OUT", "PPTUG_EF", 
+                                        "UGDS_WHITE", "PCIP27")]
+    data_for_best_model <-data.frame(lapply(data_for_best_model, as.numeric))
+    data_for_best_model <- na.omit(data_for_best_model)
+    best_model <- lm(PCIP27~poly(C150_4, 2) + poly(TUITIONFEE_IN, 1) 
+                     + poly(TUITIONFEE_OUT, 4) + poly(PPTUG_EF, 2) 
+                     + poly(UGDS_WHITE, 3), data=data_for_best_model)
     
     output$linear_regression <- renderPrint({
         good <- 1
@@ -111,6 +192,17 @@ server <- function(input, output) {
             #prints a summary of the data
             summary(linear_regression_model)
         }
+    })
+    
+    output$best_tree <- renderPrint({
+        summary(best_model)
+    })
+    output$prediction <- renderPrint({
+        predict(best_model, data.frame(C150_4 = c(gradRateToPredict()), 
+                                       TUITIONFEE_IN = c(inStateToPredict()),
+                                       TUITIONFEE_OUT = c(outStateToPredict()),
+                                       PPTUG_EF = c(partTimeToPredict()),
+                                       UGDS_WHITE = c(whiteStudentToPredict())))
     })
     
     output$tree <- renderPlot({
